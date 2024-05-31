1968a4c05dd5a3d5ef821a269aabc4d8bc22dd05
shivambu321 <shivambu321@gmail.com>
2024-05-31 15:43:06 +0530

    edited inputs

--- emu8086/largest.asm
+++ emu8086/largest.asm
@@ -1,20 +1,24 @@
 ;largest in array       
 mov 500h,5
-mov 501h,55h
-mov 502h,44h
-mov 503h,33h
-mov 504h,22h
-mov 505h,11h
-mov si,0500h
-mov cl,[si]
+mov 501h, 51h
+mov 502h, 24h
+mov 503h, 2Ch
+mov 504h, CFh
+mov 505h, 3Eh
+
+mov si, 0500h
+mov cl, [si]
+mov ch, 00
 inc si
 dec cl
-mov al,[si]
+mov al, [si]
 inc si
+
 l1:
 cmp al,[si]
 jnc skip
 mov al,[si]
+
 skip:
 inc si
 loop l1
