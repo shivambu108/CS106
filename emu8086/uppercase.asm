05cf358e2fa471da0aae8a34864f2be237b6c0f3
shivambu321 <shivambu321@gmail.com>
2024-05-31 15:58:38 +0530

    corrected logical flaws

--- emu8086/uppercase.asm
+++ emu8086/uppercase.asm
@@ -1,12 +1,14 @@
 ;conversion to uppercase
 mov cx,26
-mov si,1600h
-mov di,1700h
+mov si,1500h
+mov di,1600h
 
 convert:
 mov al,[si]
-mov al,'a'   
+cmp al,'a'   
 jb nt_lwr
+cmp al,'z'
+ja not_lwr
 sub al,20h
 
 nt_lwr:
