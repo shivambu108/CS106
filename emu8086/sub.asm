76907f281a2417f4f408d303a1094fa6771ee6e0
shivambu321 <shivambu321@gmail.com>
2024-05-31 15:36:58 +0530

    adding left out examples

--- emu8086/sub.asm
+++ emu8086/sub.asm
@@ -1,11 +1,17 @@
 ;sub 8 bit with borrow 
-mov al,28h
-mov bl,14h
+
+mov al,38h
+mov bl,21h
 sbb al,bl
-mov ds:[0200h],al
+mov ds:[0203h],al
+
+mov al,20h
+mov bl,41h
+sbb al,bl
+mov ds:[0204h],al
 
 ;sub 16 bit with borrow 
-mov ax,2128h
-mov bx,1442h
+mov ax,48E7h
+mov bx,2481h
 sbb ax,bx
-mov ds:[0200h],ax
+mov ds:[0206h],ax
